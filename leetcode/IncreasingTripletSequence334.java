cc2c1f829b0c09d61cf2d13e3d4da4c63a02bbe0
Garvit <garvit.jain@publicissapient.com>
2020-12-18 18:00:13 +0530

    jgarvitj-day3.3-leetcode

---
+++ leetcode/IncreasingTripletSequence334.java
@@ -0,0 +1,29 @@
+package com.personal.dsa.leetcode;
+
+/*
+ * Need to store only 2 elements
+ * O(n)
+ */
+
+public class IncreasingTripletSequence334 {
+
+//}
+//class Solution {
+	public boolean increasingTriplet(int[] nums) {
+		int[] arr = new int[2];
+		arr[0] = nums[0];
+		arr[1] = Integer.MAX_VALUE;
+		for (int i = 1; i < nums.length; i++) {
+			if (arr[1] < nums[i]) {
+				return true;
+			}
+			if (arr[0] < nums[i] && arr[1] > nums[i]) {
+				arr[1] = nums[i];
+			} else if (arr[0] > nums[i]) {
+				arr[0] = nums[i];
+			}
+		}
+		return false;
+
+	}
+}
