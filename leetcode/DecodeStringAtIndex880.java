5e98ec6c76a6ddeb99b8c839fa88fb4831e66b40
= <=>
2020-12-21 09:53:50 +0530

    jgarvitj-day6.1-leetcode

---
+++ leetcode/DecodeStringAtIndex880.java
@@ -0,0 +1,41 @@
+package com.personal.dsa.leetcode;
+
+public class DecodeStringAtIndex880 {
+
+//}
+//	class Solution {
+	public String decodeAtIndex(String S, int K) {
+
+		long size = 0;
+		int j = S.length() - 1;
+		for (int i = 0; i < S.length(); i++) {
+			Character c = S.charAt(i);
+			if (c >= 'a' && c <= 'z') {
+				if (size + 1 == K) {
+					return c.toString();
+				} else {
+					size++;
+				}
+			} else {
+				size = size * (c - '0');
+				if (size > K) {
+					j = i;
+					break;
+				}
+			}
+		}
+
+		for (int i = j; i >= 0; i--) {
+			K %= size;
+			if (K == 0 && S.charAt(i) >= 'a' && S.charAt(i) <= 'z') {
+				return "" + S.charAt(i);
+			}
+			if (S.charAt(i) >= '0' && S.charAt(i) <= '9') {
+				size = size / (S.charAt(i) - '0');
+			} else {
+				size--;
+			}
+		}
+		return null;
+	}
+}
