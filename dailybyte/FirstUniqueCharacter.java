fcf85ddc8de66825fed244d46527069ba5a3a18e
jgarvitj <jain.garvit4@gmail.com>
2020-12-22 11:51:22 +0530

    jgarvitj-day7.3-dailybyte

---
+++ dailybyte/FirstUniqueCharacter.java
@@ -0,0 +1,25 @@
+package com.personal.dsa.dailybyte;
+
+import java.util.HashMap;
+
+public class FirstUniqueCharacter {
+
+	private static int findFirstUniqueCharacter(String s) {
+		// TODO Auto-generated method stub
+		HashMap<Character, Integer> charCount = new HashMap<>();
+		for (Character c : s.toCharArray()) {
+			charCount.put(c, charCount.getOrDefault(c, 0) + 1);
+		}
+		for (int i = 0; i < s.length(); i++) {
+			if (charCount.get(s.charAt(i)) == 1) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(findFirstUniqueCharacter("abcabd") == 2 && findFirstUniqueCharacter("thedailybyte") == 1
+				&& findFirstUniqueCharacter("developer") == 0);
+	}
+}
