2ddbd58e6a4c9eb84c4a19344aed49cbafb88073
jgarvitj <jain.garvit4@gmail.com>
2020-12-22 14:59:35 +0530

    jgarvitj-day7.4-leetcode

---
+++ leetcode/MinNumberOfRefuelingSteps871.java
@@ -0,0 +1,37 @@
+package com.personal.dsa.leetcode;
+
+import java.util.PriorityQueue;
+
+public class MinNumberOfRefuelingSteps871 {
+
+//}
+//class Solution {
+
+	public int minRefuelStops(int target, int startFuel, int[][] stations) {
+		if (target <= startFuel) {
+			return 0;
+		}
+		PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a); // to store fuels at diff stations
+		int dist = startFuel;
+		int stationIndex = 0;
+		int count = 0;
+		int n = stations.length;
+		while (true) {
+			while (stationIndex < n && dist >= stations[stationIndex][0]) {
+				pq.add(stations[stationIndex][1]);
+				stationIndex++;
+			}
+
+			if (pq.isEmpty()) {
+				return -1;
+			}
+
+			dist = dist + pq.poll();
+			count++;
+			if (dist >= target) {
+				return count;
+			}
+		}
+
+	}
+}
