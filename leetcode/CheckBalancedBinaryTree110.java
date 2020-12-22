558e12fe4279aca244bbce70cad40498131731a8
jgarvitj <jain.garvit4@gmail.com>
2020-12-23 00:49:20 +0530

    jgarvitj-day8.1-leetcode

---
+++ leetcode/CheckBalancedBinaryTree110.java
@@ -0,0 +1,55 @@
+package com.personal.dsa.leetcode;
+
+// Definition for a binary tree node.
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode() {
+	}
+
+	TreeNode(int val) {
+		this.val = val;
+	}
+
+	TreeNode(int val, TreeNode left, TreeNode right) {
+		this.val = val;
+		this.left = left;
+		this.right = right;
+	}
+}
+
+public class CheckBalancedBinaryTree110 {
+
+//}
+//
+//class Solution {
+
+	private static int getHt(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+		int leftHt = getHt(root.left);
+		int rightHt = getHt(root.right);
+
+		if (Math.abs(leftHt - rightHt) > 1 || leftHt == -1 || rightHt == -1) {
+			return -1;
+		}
+		return Math.max(leftHt, rightHt) + 1;
+	}
+
+	public boolean isBalanced(TreeNode root) {
+
+		if (root == null) {
+			return true;
+		}
+
+		if (root.left == null && root.right == null) {
+			return true;
+		}
+
+		return getHt(root) != -1;
+
+	}
+}
