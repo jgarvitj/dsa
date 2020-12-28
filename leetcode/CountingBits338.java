9b95256f9aa854dd6608679485ca70072bfd6a68
jgarvitj <jain.garvit4@gmail.com>
2020-12-28 10:43:29 +0530

    jgarvitj-day13.1-dailybyte

---
+++ leetcode/CountingBits338.java
@@ -0,0 +1,26 @@
+package com.personal.dsa.leetcode;
+
+public class CountingBits338 {
+
+//}
+//class Solution {
+	public int[] countBits(int num) {
+		int[] arr = new int[num + 1];
+		arr[0] = 0;
+		long it1 = 1;
+		int it2 = 1;
+		int k = 1;
+		while (it2 <= num) {
+			if (it1 == it2) {
+				arr[it2] = 1;
+				it1 = it1 * 2;
+				k = 1;
+			} else {
+				arr[it2] = arr[k] + 1;
+				k++;
+			}
+			it2++;
+		}
+		return arr;
+	}
+}
