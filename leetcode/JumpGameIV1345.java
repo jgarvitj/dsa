e5176f41d53675b05f0a367a6d64db914739c021
jgarvitj <jain.garvit4@gmail.com>
2020-12-28 13:30:09 +0530

    jgarvitj-day13.3-leetcode

---
+++ leetcode/JumpGameIV1345.java
@@ -0,0 +1,41 @@
+package com.personal.dsa.leetcode;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+public class JumpGameIV1345 {
+
+//}
+//class Solution {
+	public int minJumps(int[] arr) {
+		int n = arr.length;
+		HashMap<Integer, List<Integer>> indexHm = new HashMap<>();
+		for (int i = 0; i < n; i++)
+			indexHm.computeIfAbsent(arr[i], x -> new LinkedList<>()).add(i);
+		boolean[] isVis = new boolean[n];
+		isVis[0] = true;
+		LinkedList<Integer> q = new LinkedList<>();
+		q.offer(0);
+		int step = 0;
+		while (!q.isEmpty()) {
+			for (int size = q.size(); size > 0; size--) {
+				int i = q.poll();
+				if (i == n - 1)
+					return step; // Reached to last index
+				List<Integer> nxt = indexHm.get(arr[i]);
+				nxt.add(i - 1);
+				nxt.add(i + 1);
+				for (int j : nxt) {
+					if (j >= 0 && j < n && !isVis[j]) {
+						isVis[j] = true;
+						q.offer(j);
+					}
+				}
+				nxt.clear();
+			}
+			step++;
+		}
+		return 0;
+	}
+}
