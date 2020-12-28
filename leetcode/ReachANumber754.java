e6f7a37f82d495fa1c3cb51310d225e0fd867c93
jgarvitj <jain.garvit4@gmail.com>
2020-12-28 16:59:59 +0530

    jgarvitj-day13.4-leetcode

---
+++ leetcode/ReachANumber754.java
@@ -0,0 +1,31 @@
+package com.personal.dsa.leetcode;
+
+public class ReachANumber754 {
+
+//}
+//class Solution {
+	public int reachNumber(int target) {
+		target = Math.abs(target);
+		if (target < 2) {
+			return target;
+		}
+		int k = 0;
+		int i = 1;
+		while (k < target) {
+			k = k + i;
+			i++;
+		}
+		i--;
+		if (k == target) {
+			return i;
+		}
+		if ((k - target) % 2 == 0) {
+			return i;
+		}
+		if ((k + i + 1 - target) % 2 == 0) {
+			return i + 1;
+		}
+		return i + 2;
+
+	}
+}
