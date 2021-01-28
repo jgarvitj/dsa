259fcaf386575e94e69d08503f16941eab6a59b5
jgarvitj <jain.garvit4@gmail.com>
2021-01-28 14:09:34 +0530

    day1.52-leetcode

--- leetcode/_238_ProductWithoutSelf.java
+++ leetcode/_238_ProductWithoutSelf.java
@@ -4,25 +4,25 @@ public class _238_ProductWithoutSelf {
 
 //}
 //class Solution {
-    public int[] productExceptSelf(int[] nums) {
-        
-        int[] prod = new int[nums.length];
-        //int rightProd = Arrays.stream(nums).reduce(1, (a,b) -> a*b);
-        if(nums.length == 0) {
-            return prod;
-        }
-        prod[0] = 1;
-        for(int i = 1; i < nums.length; i++) {
-            prod[i] = prod[i-1] * nums[i-1];
-        }
-        
-        int rightProd = 1;
-        for(int i = nums.length - 1; i >= 0; i--) {
-            prod[i] = prod[i] * rightProd;
-            rightProd = rightProd * nums[i];
-        }
-        
-        return prod;
-        
-    }
+	public int[] productExceptSelf(int[] nums) {
+
+		int[] prod = new int[nums.length];
+		// int rightProd = Arrays.stream(nums).reduce(1, (a,b) -> a*b);
+		if (nums.length == 0) {
+			return prod;
+		}
+		prod[0] = 1;
+		for (int i = 1; i < nums.length; i++) {
+			prod[i] = prod[i - 1] * nums[i - 1];
+		}
+
+		int rightProd = 1;
+		for (int i = nums.length - 1; i >= 0; i--) {
+			prod[i] = prod[i] * rightProd;
+			rightProd = rightProd * nums[i];
+		}
+
+		return prod;
+
+	}
 }
